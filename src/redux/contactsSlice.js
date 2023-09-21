02bcecc178053d65ed80adca0a52533efc00f575
victoria2588 <123986940+victoria2588@users.noreply.github.com>
2023-09-21 14:06:18 +0300

    commit 006
    fix

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -27,6 +27,7 @@ const handleDeleteContactsFulfilled = (state, action) => {
   const index = state.items.findIndex(
     contact => contact.id === action.payload.id
   );
+  state.items.splice(index, 1);
 };
 const contactsSlice = createSlice({
   name: 'contacts',
